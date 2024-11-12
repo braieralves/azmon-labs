76f73cbb6e8058ab4cfb41743d5b38b64530a2bd
tiagojfernandes <103953307+tiagojfernandes@users.noreply.github.com>
2024-11-12 17:12:57 +0000

    Update init-lab.sh

--- init-lab.sh
+++ init-lab.sh
@@ -115,7 +115,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_name
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "aks${var.aks_name}"
+  dns_prefix          = "akslab"
 
   default_node_pool {
     name       = "default"
