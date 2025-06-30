328732cae721d9eeedc7cb7d7fa0dbe5f84f1591
Tiago Fernandes <tifernan@microsoft.com>
2025-06-30 14:11:02 +0100

    update deploy-monitoring and outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,16 @@
-# Shared output definitions
+# terraform/outputs.tf
+
+output "resource_group_name" {
+  description = "The name of the resource group"
+  value       = module.resource_group.name
+}
+
+output "log_analytics_workspace_id" {
+  description = "The full resource ID of the Log Analytics Workspace"
+  value       = module.log_analytics.workspace_id
+}
+
+output "log_analytics_workspace_name" {
+  description = "The name of the Log Analytics Workspace"
+  value       = module.log_analytics.name
+}
